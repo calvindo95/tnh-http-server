019f9f5b1c8756330932d6ec5fb5838f39cee7e1
calvindo95 <calvindo95@gmail.com>
2023-08-28 18:43:23 -0500

    Fixed json logic in resource

--- include/Resource.h
+++ include/Resource.h
@@ -20,11 +20,11 @@ class post_json : public httpserver::http_resource, public HTTPResources {
         std::shared_ptr<httpserver::http_response> render(const httpserver::http_request& req);
     private:
         // "DeviceID", "hash", "CurrentDateTime", "Temperature", "Humidity"
-        nlohmann::json m_json;
+        const std::vector<std::string> m_json_keys{"DeviceID", "hash", "CurrentDateTime", "Temperature", "Humidity"};
 
         // Function to consume items in tsqueue
         void consume_thread()noexcept;
 
         // Parse csv into std::map<std::string,std::string>
-        int parse_json(std::string json_string);
+        int parse_json(std::string json_string, nlohmann::json& json);
 };
--- src/Resource.cpp
+++ src/Resource.cpp
@@ -26,7 +26,8 @@ post_json::post_json(){
 std::shared_ptr<httpserver::http_response> post_json::render(const httpserver::http_request& req) {
     int ret_val = 0; 
     std::stringstream ss;
-
+    nlohmann::json tmp_j;
+    
     std::map<std::string_view, std::string_view, httpserver::http::header_comparator> headers;
     headers = req.get_headers();
 
@@ -39,32 +40,31 @@ std::shared_ptr<httpserver::http_response> post_json::render(const httpserver::h
     std::string tmp = std::string(req.get_content());
 
     // Parse json string into json object
-    ret_val += parse_json(tmp);
+    ret_val += parse_json(tmp,tmp_j);
 
     if(ret_val == 0){
-        ss << "Inserting data into queue: " << tmp;
+        ss << "Inserting data into queue: " << tmp_j;
         m_logger.log_trace(ss.str(), "GENTRACE");
-        m_tsq.push(m_json);
+        m_tsq.push(tmp_j);
     }
 
     return std::shared_ptr<httpserver::http_response>(new httpserver::string_response("Received data value: " + std::to_string(ret_val) + "\n"));
 }
 
-int post_json::parse_json(std::string json_string){
+int post_json::parse_json(std::string json_string, nlohmann::json& json){
     std::stringstream ss;
-    m_json = nlohmann::json::parse(json_string);
+    json = nlohmann::json::parse(json_string);
 
     // Check if json has correct number of key/values
-    if(m_json.size() != 5){
+    if(json.size() != 5){
         m_logger.log_trace("Input json does not have correct size(5)", "GENTRACE");
         return 1;
     }
 
     // Check if correct keys
-    std::vector<std::string> tmp_vec{"DeviceID", "hash", "CurrentDateTime", "Temperature", "Humidity"};
-    for(int i = 0; i < tmp_vec.size(); i++){
-        if(m_json.count(tmp_vec[i]) != 1){
-            ss << "JSON key value: '" << tmp_vec[i] << "' is not present in post json";
+    for(int i = 0; i < m_json_keys.size(); i++){
+        if(json.count(m_json_keys[i]) != 1){
+            ss << "JSON key value: '" << m_json_keys[i] << "' is not present in post json";
             m_logger.log_trace(ss.str(), "GENTRACE");
             return 1;
         }
@@ -74,6 +74,7 @@ int post_json::parse_json(std::string json_string){
 
 void post_json::consume_thread() noexcept{
     while(true){
+        int ret_val = 0;
         std::stringstream ss;
         std::stringstream ssq1;
         std::stringstream ssq2;
@@ -85,8 +86,16 @@ void post_json::consume_thread() noexcept{
         ssq1 << "INSERT INTO History (Temperature, Humidity) VALUES(" << j["Temperature"] << "," << j["Humidity"] << ")";
         ssq2 << "INSERT INTO Data_History (DeviceID, HistoryID, CurrentDateTime) VALUES (" << j["DeviceID"] << ",LAST_INSERT_ID()," << j["CurrentDateTime"] << ")";
 
-        dbq.insert(ssq1.str());
-        dbq.insert(ssq2.str());
+        ret_val += dbq.insert(ssq1.str());
+        ret_val += dbq.insert(ssq2.str());
+
+        if(ret_val != 0){
+            ss << "Error inserting json data: " << j;
+            m_logger.log_trace(ss.str(), "GENTRACE");
+        }
+
+        ss.clear();
+        ss.str(std::string());
 
         ss << "Queue size reduced by 1 to: " << m_tsq.size();
         m_logger.log_trace(ss.str(), "GENTRACE");
