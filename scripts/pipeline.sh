bf48ecd6e4f11dd6fd67d16180e79a89c0b29fc1
calvindo95 <calvindo95@gmail.com>
2023-11-16 16:14:16 -0600

    Updated pipeline script

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,4 +1,25 @@
-sudo sh scripts/build_img.sh scripts/Dockerfile_arm tnh-server-arm && \
-sudo sh scripts/stop.sh tnh-server && \
-sudo docker container prune && \
-sudo sh scripts/spinup.sh tnh-server-arm 8081 tnh-server $1
+if [ $# -lt 1 ]; then # checks if number of arguments is less than 2
+    echo "Less than 1 arguments were supplied"
+    echo "Usage: 'sh pipeine.sh <DB_PASSWORD>"
+    return 1
+fi
+
+if $(docker image ls | grep -q tnh-base-arm)
+then
+    echo "tnh-base-arm img exists, building tnh-server-arm img"
+    sh scripts/build_img.sh scripts/Dockerfile_arm tnh-server-arm
+
+    if $(docker ps | grep -q tnh-server)
+    then
+        echo "tnh-server still active, stopping."
+        sh scripts/stop.sh tnh-server
+    else
+        echo "tnh-server already stopped."
+    fi
+
+    yes | docker container prune
+
+    sh scripts/spinup.sh tnh-server-arm 8081 tnh-server $1
+else
+    echo "tnh-base-arm img does not exist"
+fi
