395c018b05c74de61e84749ce5b45238c4c1c07f
Calvin <calvindo95@gmail.com>
2025-01-21 21:28:06 -0500

    Updated sql queries to match new DB schema

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -81,16 +81,15 @@ void post_json::consume_thread() noexcept{
         // Wait for first message
         nlohmann::json j = m_tsq.pop(queue_size);
 
-        ssq << "INSERT INTO History (Temperature, Humidity) VALUES(" << j["Temperature"] << "," << j["Humidity"] << "); \
-        INSERT INTO Data_History (DeviceID, HistoryID, CurrentDateTime) VALUES (" << j["DeviceID"] << ",LAST_INSERT_ID()," << j["CurrentDateTime"] << ");";
+        ssq << "INSERT INTO History (Temperature, Humidity,DeviceID, CurrentDateTime) VALUES(" << j["Temperature"] << "," << j["Humidity"] << ","<< j["DeviceID"] << j["CurrentDateTime"] << ");";
 
         // If many messages in queue, add multiple inserts to query
         for(int i = 0; i < queue_size; i++){
             std::stringstream ssq_temp;
             nlohmann::json j_temp = m_tsq.pop();
 
-            ssq_temp << "INSERT INTO History (Temperature, Humidity) VALUES(" << j_temp["Temperature"] << "," << j_temp["Humidity"] << "); \
-            INSERT INTO Data_History (DeviceID, HistoryID, CurrentDateTime) VALUES (" << j_temp["DeviceID"] << ",LAST_INSERT_ID()," << j_temp["CurrentDateTime"] << ");";
+            ssq_temp << "INSERT INTO History (Temperature, Humidity,DeviceID, CurrentDateTime) VALUES(" << j_temp["Temperature"] << "," << j_temp["Humidity"] << ","<< j_temp["DeviceID"] << j_temp["CurrentDateTime"] << ");";
+
 
             ssq << ssq_temp.str();
         }
